69e597b89bf31df32595eaaf2f2b2800b48fdb63
Léo Coletta <mail.leo.coletta@gmail.com>
2025-09-19 20:09:49 +0200

    fix: render Navbar inside route so <A> works
    - Use layout route (MainLayout) and nested routes
    
    - Re-enable <A> links in Navbar; remove Router root wrapper

--- src/App.tsx
+++ src/App.tsx
@@ -1,32 +1,29 @@
 import type { Component } from 'solid-js';
 import { Route } from '@solidjs/router';
-import Navbar from './components/Navbar';
-import Footer from './components/Footer';
 import Home from './pages/Home';
 import ResourceList from './pages/ResourceList';
 import ResourceDetail from './pages/ResourceDetail';
+import MainLayout from './layouts/MainLayout';
 
 const App: Component = () => {
   return (
-    <div class="min-h-dvh bg-gray-50 text-gray-900">
-      <Navbar />
-      <main class="mx-auto max-w-6xl px-4 py-6">
-          <Route path="/" component={Home} />
-          <Route path="/pokemon" component={() => <ResourceList resource="pokemon" />} />
-          <Route path="/pokemon/:id" component={() => <ResourceDetail resource="pokemon" />} />
-          <Route path="/pokemon-species" component={() => <ResourceList resource="pokemon-species" />} />
-          <Route path="/pokemon-species/:id" component={() => <ResourceDetail resource="pokemon-species" />} />
-          <Route path="/move" component={() => <ResourceList resource="move" />} />
-          <Route path="/move/:id" component={() => <ResourceDetail resource="move" />} />
-          <Route path="/ability" component={() => <ResourceList resource="ability" />} />
-          <Route path="/ability/:id" component={() => <ResourceDetail resource="ability" />} />
-          <Route path="/type" component={() => <ResourceList resource="type" />} />
-          <Route path="/type/:id" component={() => <ResourceDetail resource="type" />} />
-          <Route path="/evolution-chain" component={() => <ResourceList resource="evolution-chain" />} />
-          <Route path="/evolution-chain/:id" component={() => <ResourceDetail resource="evolution-chain" />} />
-      </main>
-      <Footer />
-    </div>
+    <>
+      <Route path="/" component={MainLayout}>
+        <Route path="/" component={Home} />
+        <Route path="pokemon" component={() => <ResourceList resource="pokemon" />} />
+        <Route path="pokemon/:id" component={() => <ResourceDetail resource="pokemon" />} />
+        <Route path="pokemon-species" component={() => <ResourceList resource="pokemon-species" />} />
+        <Route path="pokemon-species/:id" component={() => <ResourceDetail resource="pokemon-species" />} />
+        <Route path="move" component={() => <ResourceList resource="move" />} />
+        <Route path="move/:id" component={() => <ResourceDetail resource="move" />} />
+        <Route path="ability" component={() => <ResourceList resource="ability" />} />
+        <Route path="ability/:id" component={() => <ResourceDetail resource="ability" />} />
+        <Route path="type" component={() => <ResourceList resource="type" />} />
+        <Route path="type/:id" component={() => <ResourceDetail resource="type" />} />
+        <Route path="evolution-chain" component={() => <ResourceList resource="evolution-chain" />} />
+        <Route path="evolution-chain/:id" component={() => <ResourceDetail resource="evolution-chain" />} />
+      </Route>
+    </>
   );
 };
 
